76d868e7507516f39d2d37f1f22827bdd235c599
Nathnael Mekonnen <nmktadesse@gmail.com>
2022-12-23 16:31:44 +0300

    Time: 63 ms (19.56%), Space: 13.8 MB (96.75%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        stack = []
+        ans = ""
+        for i, char in enumerate(command):
+            if char == "G":
+                ans += "G"
+            elif char == ")":
+                if stack.pop() == "(":
+                    ans += "o"
+                else: 
+                    ans += "al"
+                    stack.pop()
+                    stack.pop()
+            else: stack.append(char)
+                
+        print(ans)
+        return ans
