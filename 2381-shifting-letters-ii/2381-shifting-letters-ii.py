fe46a3ea0638174338c0f49e3ce0b395a8cad483
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-02-23 17:15:22 +0300

    Time: 1403 ms (47.48%), Space: 39.6 MB (16.20%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        presum = [0] * (len(s) + 1)
+        
+        for val in shifts:
+            left, right, direction = val
+            
+            if direction == 0:
+                presum[left] -= 1
+                presum[right+1] += 1
+                
+            else:
+                presum[left] += 1
+                presum[right+1] -= 1
+                
+        for i in range(1, len(presum)):
+            presum[i] += presum[i-1]
+            
+        ans = []
+        
+        print(presum)
+                
+        for i in range(len(presum)-1):
+            ans.append(chr(((ord(s[i]) - 97 + presum[i]) % 26) + 97))
+        
+        return "".join(ans)
