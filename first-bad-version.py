6af54f5b085b4bcf9dec4a40f5c935944f219087
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-03-06 11:23:23 +0300

    added first-bad-version.python

---
+++ first-bad-version.py
@@ -0,0 +1,17 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r, ans = 1, n, None
+        
+        while l <= r:
+            m = l + (r - l) // 2
+
+            if isBadVersion(m):
+                ans = m
+                r = m - 1
+            else:
+                l = m + 1
+
+        return ans
