12fd11b1f85b4a8fdf6af441c4f01890bfc5da9e
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-04-28 18:45:49 +0300

    added race-car.python

--- race-car.py
+++ race-car.py
@@ -12,21 +12,23 @@ class Solution:
             for _ in range(cl):
                 pos, speed = queue.popleft()
                 
+
                 if pos == target:
                     return count
 
-                if (pos + speed, speed * 2) not in visited:
-                    queue.append((pos + speed, speed * 2))
-                    visited.add((pos + speed, speed * 2))
+                if 2 * target > pos > -2 * target:
+                    if (pos + speed, speed * 2) not in visited:
+                        queue.append((pos + speed, speed * 2))
+                        visited.add((pos + speed, speed * 2))
 
-                if speed > 0:
-                    if (pos, -1) not in visited:
-                        queue.append((pos, -1))
-                        visited.add((pos, -1))
-                else:
-                    if (pos, 1) not in visited:
-                        queue.append((pos, 1))
-                        visited.add((pos, 1))
+                    if speed > 0:
+                        if (pos, -1) not in visited:
+                            queue.append((pos, -1))
+                            visited.add((pos, -1))
+                    else:
+                        if (pos, 1) not in visited:
+                            queue.append((pos, 1))
+                            visited.add((pos, 1))
 
             count += 1
 
