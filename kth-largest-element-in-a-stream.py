b336719eadeefa4ecbf94105c2f944f40dc63d2b
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-05-07 11:02:39 +0300

    added kth-largest-element-in-a-stream.python

---
+++ kth-largest-element-in-a-stream.py
@@ -0,0 +1,24 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+
+        heapq.heapify(nums)
+        
+        for _ in range(k):
+            if nums:
+                heapq.heappush(self.heap, nums.pop())
+        
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)   
+
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+            
+        return self.heap[0]     
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
