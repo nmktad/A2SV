e3bff13dea7161acb4f3118457d6a379a948ea1c
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-08-08 07:18:07 +0300

    added longest-arithmetic-subsequence-of-given-difference.python

---
+++ longest-arithmetic-subsequence-of-given-difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        memo = defaultdict(int)
+
+        for num in arr:
+            memo[num] = memo[num-difference] + 1
+
+        return max(memo.values())
