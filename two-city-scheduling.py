b52d98f1b6301bd7294b29a37def9b897d81d6e7
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-10-03 14:50:49 +0300

    added two-city-scheduling.python

---
+++ two-city-scheduling.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        ans = sum([i for i, _ in costs])
+
+        costs = sorted([(j - i, i, j) for i, j in costs])
+
+        for i in range(len(costs)//2):
+            ans -= costs[i][1]
+            ans += costs[i][2]
+        return ans
