1cf9db77349a561697f71375ee0aad1f42a75b70
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-10-12 15:58:45 +0300

    added flip-equivalent-binary-trees.python

--- flip-equivalent-binary-trees.py
+++ flip-equivalent-binary-trees.py
@@ -8,40 +8,12 @@ class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         if not root1 and not root2:
             return True
-        
-        if not root1 and root2 or not root2 and root1:
+        if not root1 or not root2:
+            return False
+        if root1.val !=root2.val:
             return False
         
-        child = defaultdict(set)
-
-        queue = deque([root1])
-
-        while queue:
-            curr = queue.popleft()
-            
-            if curr.left:
-                child[curr.val].add(curr.left.val)
-                queue.append(curr.left)
-
-            if curr.right:
-                child[curr.val].add(curr.right.val)
-                queue.append(curr.right)
-
-        queue.append(root2)
-
-        while queue:
-            curr = queue.popleft()
-            temp = set()
-
-            if curr.left:
-                temp.add(curr.left.val)
-                queue.append(curr.left)
-
-            if curr.right:
-                temp.add(curr.right.val)
-                queue.append(curr.right)
-
-            if temp != child[curr.val]:
-                return False
-
-        return True
+        result= self.flipEquiv(root1.left,root2.left) and self.flipEquiv(root1.right,root2.right)
+        if not result:
+            result = self.flipEquiv(root1.left,root2.right) and self.flipEquiv(root1.right,root2.left) 
+        return result
