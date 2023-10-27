a77c1c787aa040ec4bb9c635e09e4a333b5acdd7
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-10-27 18:43:27 +0300

    added two-sum.python

---
+++ two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], tar: int) -> List[int]:
+        ind = {}
+
+        for i, val in enumerate(nums):
+            if tar - val in ind:
+                return [i, ind[tar-val]]
+
+            ind[val] = i
