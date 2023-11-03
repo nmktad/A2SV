d6172fba2559a37bc9a786ba3bd26103921bd667
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-11-03 15:23:30 +0300

    Added solution for Number of Good Pairs

---
+++ aftercamp/number-of-good-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count  = defaultdict(int)
+        ans = 0
+        for num in nums:
+            ans += count[num]
+            count[num] += 1        
+        return ans
+
+        
