fe40aa2599dd93a568eae40fcbfb9cb46c2bd05a
Nathnael Mekonnen <nmktadesse@gmail.com>
2023-11-12 12:03:48 +0300

    Added solution for Find Peak Element

--- aftercamp/find-peak-element.py
+++ aftercamp/find-peak-element.py
@@ -5,22 +5,18 @@ class Solution:
 
         while l <= r:
             mid = l + (r-l)//2
-            if 0 < mid < len(nums)-1:
-                if nums[mid-1] < nums[mid] > nums[mid+1]:
-                    return mid
 
-                elif nums[mid] < nums[mid+1]:
-                    l = mid+1
-                else:
-                    r = mid-1
+            if mid == 0:
+                return mid if nums[mid] > nums[mid+1] else mid + 1
 
-            elif mid == 0:
-                if nums[mid] > nums[mid+1]:
-                    return mid
-                else:
-                    return mid + 1
-            elif mid == len(nums)-1:
-                if nums[mid] > nums[mid-1]:
-                    return mid
-                else:
-                    return mid-1
+            if mid == len(nums)-1:
+                return mid if nums[mid] > nums[mid-1] else mid-1
+
+            if nums[mid-1] < nums[mid] > nums[mid+1]:
+                return mid
+
+            if nums[mid] < nums[mid+1]:
+                l = mid+1
+            else:
+                r = mid-1
+                
